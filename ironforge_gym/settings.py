4d0bc6e4d051b48530dac331c341c3e7d31e2cbb
Sabbir <mrtushar858@gmail.com>
2026-05-28 00:53:29 +0600

    IronForge Gym - initial commit

--- ironforge_gym/settings.py
+++ ironforge_gym/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ironforge-change-in-p
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,.onrender.com').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
